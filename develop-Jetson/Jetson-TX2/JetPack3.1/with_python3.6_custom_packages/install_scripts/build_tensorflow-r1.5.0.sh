14c7367f44f7a07bcc297812715dd04e0cb721a7
Akira Sasaki <gclue.akira@gmail.com>
2018-07-05 09:49:02 +0900

    Update develop jetson.

---
+++ develop-Jetson/Jetson-TX2/JetPack3.1/with_python3.6_custom_packages/install_scripts/build_tensorflow-r1.5.0.sh
@@ -0,0 +1,76 @@
+########################################
+# TensorFlow r1.5.0
+########################################
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+# TensorFlow r1.5.0 download
+
+#Cuda compilation tools, release 9.0, V9.0.252
+#9.0.252
+
+mkdir -p /compile \
+&& cd /compile \
+&& git clone -b r1.5 https://github.com/tensorflow/tensorflow
+
+# CUDA,CUDNNバージョン確認
+cat /usr/local/cuda/version.txt
+nvcc --version
+grep "#define CUDNN_MAJOR" -A2 /usr/include/cudnn.h
+#TF_CUDA_VERSION=8.0 (lib 8.0.84, nvcc 8.0.72)
+#TF_CUDNN_VERSION=6.0.21
+# CUDAはx.xまでを指定可能
+# CUDNNはx.x.xまで指定可能
+
+# build/install Python API
+cd /compile/tensorflow
+env CI_BUILD_PYTHON=python \
+    LD_LIBRARY_PATH=/usr/local/cuda/extras/CUPTI/lib64:${LD_LIBRARY_PATH} \
+    CUDA_TOOLKIT_PATH=/usr/local/cuda \
+    CUDNN_INSTALL_PATH=/usr/lib/x86_64-linux-gnu \
+    GCC_HOST_COMPILER_PATH=/usr/bin/gcc \
+    PYTHON_BIN_PATH=/usr/bin/python \
+    PYTHON_LIB_PATH=/usr/local/lib/python3.6/dist-packages \
+    CC_OPT_FLAGS='-march=native' \
+    TF_NEED_JEMALLOC=1 \
+    TF_NEED_GCP=0 \
+    TF_NEED_HDFS=0 \
+    TF_NEED_S3=0 \
+    TF_ENABLE_XLA=0 \
+    TF_NEED_GDR=0 \
+    TF_NEED_VERBS=0 \
+    TF_NEED_OPENCL=0 \
+    TF_NEED_OPENCL_SYCL=0 \
+    TF_SET_ANDROID_WORKSPACE= \
+    TF_NEED_CUDA=1 \
+    TF_CUDA_CLANG=0 \
+    TF_CUDA_COMPUTE_CAPABILITIES=6.2 \
+    TF_CUDA_VERSION=8.0 \
+    TF_CUDNN_VERSION=`grep "#define CUDNN_MAJOR" -A2 /usr/include/cudnn.h | sed -e 's/\#define\s[A-Z_]*\s*\(.*\)$/\1/g' | sed -e ':loop; N; $!b loop; s/\n/./g'` \
+    TF_NEED_MPI=0 \
+    ./configure
+
+# TX2は、メモリ消費量を抑えたいのでOpenMPI,GCP,HDFS,S3,XLAを無効にする
+
+# IntelじゃないのでMKLは使わない
+time bazel build --config=cuda --config="opt" --copt='-march=native' --copt="-O3" --verbose_failures --subcommands //tensorflow/tools/pip_package:build_pip_package \
+&& bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
+
+mkdir -p $SCRIPT_DIR/../binary
+mv -f /tmp/tensorflow_pkg/tensorflow-1.5.0-cp36-cp36m-linux_aarch64.whl $SCRIPT_DIR/../binary
+
+
+# benchmark
+cd /compile/tensorflow/tools
+git clone https://github.com/tensorflow/tensorflow/tree/master/tensorflow/tools/benchmark
+cd /compile/tensorflow
+time bazel build --config=cuda --config="opt" --copt='-march=native' --copt="-O3" --verbose_failures --subcommands //tensorflow/tools/benchmark:benchmark_model
+
+cd /compile/tensorflow/bazel-bin/tensorflow/tools/benchmark
+wget --no-check-certificate https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip
+unzip inception5h.zip
+
+/compile/tensorflow/bazel-bin/tensorflow/tools/benchmark/benchmark_model \
+  --graph=tensorflow_inception_graph.pb \
+  --input_layer="input:0" \
+  --input_layer_shape="1,224,224,3" \
+  --input_layer_type="float" \
+  --output_layer="output:0"
---
+++ develop-Jetson/README.md
@@ -0,0 +1,14 @@
+# Jetson TX2 環境構築
+
+## [JetPack 3.1 環境設定]
+* CPUファン自動起動
+* SPI有効化
+* Python 3.6
+* OpenCV 3.4.0
+* Tensorflow r1.6.0
+
+## [JetPack 3.2 環境設定]
+* CPUファン自動起動
+* Python 3.6
+* OpenCV 3.4.1
+* Tensorflow r1.4.1
